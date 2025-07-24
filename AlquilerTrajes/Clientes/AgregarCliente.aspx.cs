742c1731449f21054d69dbb9526f7ace8111ac4c
ChicoBoris <137181653+ChicoBoris@users.noreply.github.com>
2025-07-24 09:24:05 -0500

    Update AgregarCliente.aspx.cs

--- AlquilerTrajes/Clientes/AgregarCliente.aspx.cs
+++ AlquilerTrajes/Clientes/AgregarCliente.aspx.cs
@@ -37,13 +37,21 @@ namespace AlquilerTrajes.Admin
                     using (MySqlCommand cmd = new MySqlCommand("AgregarCliente", con))
                     {
                         cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        
                         cmd.Parameters.AddWithValue("@_nombres", nombres);
+                        
                         cmd.Parameters.AddWithValue("@_apellidos", apellidos);
+                        
                         cmd.Parameters.AddWithValue("@_cedula", cedula);
+                        
                         cmd.Parameters.AddWithValue("@_email", email);
+                        
                         cmd.Parameters.AddWithValue("@_telefono", telefono);
+                        
                         cmd.Parameters.AddWithValue("@_contrasena", contrasena);
+                        
                         cmd.Parameters.AddWithValue("@_direccion", direccion);
+                        
                         cmd.Parameters.AddWithValue("@_estado", estado);
 
                         con.Open();
